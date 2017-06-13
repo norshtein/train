cab8361ac5ab195c8e5c21b14a8201a522bb14ca
norshtein <1452707@tongji.edu.cn>
2017-06-13 15:49:23 +0800

    kickstart_r2

--- kickstart_r2_B/main.cpp
+++ kickstart_r2_B/main.cpp
@@ -4,33 +4,14 @@ using namespace std;
 
 const int MAXN = 3005;
 int c[MAXN][MAXN];
+int dp[MAXN][MAXN];
 int R,C,K;
 set<pair<int,int> > s;
 
-int lowbit(int x)
-{
-    return x & -x;
-}
-
-void update(int x,int y,int delta)
-{
-    for(int i = x;i <= R;i += lowbit(i))
-        for(int j = y;j <= C;j += lowbit(j))
-            c[i][j] += delta;
-}
-
-int getSum(int x,int y)
-{
-    int ans = 0;
-    for(int i = x;i;i -= lowbit(i))
-        for(int j = y;j;j -= lowbit(j))
-            ans += c[i][j];
-    return ans;
-}
 
 int get(int x1,int y1,int x2,int y2)
 {
-    return getSum(x2,y2) - getSum(x2,y1 - 1) - getSum(x1 - 1,y2) + getSum(x1 - 1,y1 - 1);
+    return dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
 }
 
 int main()
@@ -43,15 +24,23 @@ int main()
     {
         cerr << caseNum << endl;
         memset(c,0,sizeof(c));
-        s.clear();
+        memset(dp,0,sizeof(dp));
         scanf("%d%d%d",&R,&C,&K);
 
         int a,b;
         for(int i = 0;i < K;i++)
         {
             scanf("%d%d",&a,&b);
-            update(++a,++b,1);
-            s.insert(make_pair(a,b));
+            c[++a][++b] = 1;
+        }
+
+        for(int i = 1;i <= R;i++)
+        {
+            for(int j = 1;j <= C;j++)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + (c[i][j] == 1);
+                //cout << i << ' ' << j << ' ' << dp[i][j] << endl;
+            }
         }
 
         long long answer = 0;
@@ -60,10 +49,11 @@ int main()
         {
             for(int j = 1;j <= C;j++)
             {
-                if(i % 100 == 0 && j % 100 == 0)
-                    cerr << i << ' ' << j << endl;
-                if(s.count(make_pair(i,j)))
+                //if(i % 100 == 0 && j % 100 == 0)
+                    //cerr << i << ' ' << j << endl;
+                if(c[i][j])
                     continue;
+
                 int x1 = i,y1 = j;
 
                 int l = 1,r = min(R - i + 1,C - j + 1);
--- kickstart_r2_D/main.cpp
+++ kickstart_r2_D/main.cpp
@@ -7,8 +7,14 @@ int N,L1,R1,A,B,C1,C2,M;
 pair<int,int> interval[MAXN];
 vector<int> nums;
 int numCnt;
+int prefix[MAXN * 2 + 5];
+int len[MAXN * 2 + 5];
+int pre[MAXN * 2 + 5];
 
-
+int getId(int num)
+{
+    return lower_bound(nums.begin(),nums.end(),num) - nums.begin();
+}
 int main()
 {
     freopen("in.txt","r",stdin);
@@ -17,11 +23,18 @@ int main()
     scanf("%d",&T);
     for(int caseNum = 1;caseNum <= T;caseNum++)
     {
+        cerr << caseNum << endl;
+        nums.clear();
+        memset(prefix,0,sizeof(prefix));
+        memset(len,0,sizeof(len));
+        memset(pre,0,sizeof(pre));
+
         scanf("%d%d%d%d%d%d%d%d",&N,&L1,&R1,&A,&B,&C1,&C2,&M);
         int prex = interval[0].first = L1;
         int prey = interval[0].second = R1;
-        //nums.push_back(L1);
-        //nums.push_back(R1);
+        interval[0].second++;
+        nums.push_back(L1);
+        nums.push_back(R1 + 1);
         for(int i = 1;i < N;i++)
         {
             interval[i].first = ((long long)A * prex + (long long)B*prey + C1) % M;
@@ -30,38 +43,66 @@ int main()
             prey = interval[i].second;
             if(interval[i].first > interval[i].second)
                 swap(interval[i].first,interval[i].second);
-            //nums.push_back(interval[i].first);
-            //nums.push_back(interval[i].second);
+            nums.push_back(interval[i].first);
+            nums.push_back(++interval[i].second);
             //cout << interval[i].first << ' ' << interval[i].second << endl;
         }
+        nums.push_back(0);
 
-        //sort(nums.begin(),nums.end());
-        //numCnt = unique(nums.begin(),nums.end()) - nums.begin();
+        sort(nums.begin(),nums.end());
+        numCnt = unique(nums.begin(),nums.end()) - nums.begin();
+        nums.resize(numCnt);
         sort(interval,interval + N);
+
         //for(int i = 0;i < N;i++)
             //cout << interval[i].first << ' ' << interval[i].second << endl;
+        for(int i = 0;i < numCnt - 1;i++)
+        {
+            len[i] = nums[i + 1] - nums[i];
+            //cout << nums[i] << ' ' << nums[i + 1] << ' '  << len[i] << endl;
+        }
+
+        for(int i = 0;i < N;i++)
+        {
+            prefix[getId(interval[i].first)] += 1;
+            prefix[getId(interval[i].second)] -= 1;
+        }
+
+        int now = prefix[0];
+        pre[0] = 0;
+        for(int i = 1;i < numCnt;i++)
+        {
+            pre[i] += pre[i - 1];
+            if(now == 1)
+                pre[i] += len[i - 1];
+            now += prefix[i];
+            //cout << i << ' ' << pre[i] << endl;
+        }
+
+        int totalLen = 0;
+        int justNow = -1;
+        for(int i = 0;i < N;i++)
+        {
+            if(interval[i].first >= justNow)
+            {
+                totalLen += interval[i].second - interval[i].first;
+                justNow = interval[i].second;
+            }
+            else if(interval[i].second > justNow)
+            {
+                totalLen += interval[i].second - justNow;
+                justNow = interval[i].second;
+            }
+        }
+        //cout << totalLen << endl;
+
         int ans = 2e9;
         //Ã¶¾ÙÈ¥µôµÄ
         for(int dis = 0;dis < N;dis++)
         {
-            int now = 0;
-            int pre = -1;
-            for(int i = 0;i < N;i++)
-            {
-                if(i == dis)
-                    continue;
-                if(interval[i].first > pre)
-                {
-                    now += interval[i].second - interval[i].first + 1;
-                    pre = interval[i].second;
-                }
-                else if(interval[i].second > pre)
-                {
-                    now += interval[i].second - pre;
-                    pre = interval[i].second;
-                }
-            }
-            ans = min(ans,now);
+            int l = getId(interval[dis].first);
+            int r = getId(interval[dis].second);
+            ans = min(ans,totalLen - pre[r] + pre[l]);
         }
         printf("Case #%d: %d\n",caseNum,ans);
     }
